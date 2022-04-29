137c87b745d48e4e59a284af18e612fb19d8722e
elee2001 <elee2001@students.cs.ubc.ca>
2022-04-28 22:17:17 -0700

    Increase currency entry size and clean up SQL setup file)

--- client/cryptoapp/src/components/inputEntry.js
+++ client/cryptoapp/src/components/inputEntry.js
@@ -12,6 +12,7 @@ const InputEntry = () => {
         e.preventDefault();
         try {
             // TODO: do some basic client side validation of inputs
+
             const reqBody = { buy_price, coin_name, quantity, user };
             await fetch(baseURL + "entries", {
                 method: "POST",
