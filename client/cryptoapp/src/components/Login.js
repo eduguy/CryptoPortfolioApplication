331bc08b2b65583a63ab431da3f59301e73d8eb6
elee2001 <elee2001@students.cs.ubc.ca>
2022-08-07 22:04:31 -0700

    Setup pipeline

--- client/cryptoapp/src/components/Login.js
+++ client/cryptoapp/src/components/Login.js
@@ -9,7 +9,6 @@ const Login = ({ children }) => {
     const [displayedUser, setDisplayedUser] = useState("");
     const loginUser = async (e) => {
         try {
-            console.log("test print statement")
             if (displayedUser) {
                 throw "Sign out of the current user first";
             }
